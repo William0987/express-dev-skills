f4c85c1e1cb04a59e45bfaeec683929d335947b5
William0987 <williamwsianto@gmail.com>
2023-07-14 10:27:40 -0400

    finish lab 2

--- controllers/skills.js
+++ controllers/skills.js
@@ -3,18 +3,53 @@ const Skill = require('../models/skill');
 
 function index(req, res) {
     res.render('skills/index', {
-      skills: Skill.getAll()
+      skills: Skill.getAll(),
+      title: 'All Skills'
     });
   }
 
 function show(req, res) {
     res.render('skills/show', {
     skill: Skill.getOne(req.params.id),
+    title: 'Skill Details'
     });
+}
+
+function newSkill(req, res) {
+  res.render('skills/new', { title: 'New Skill' });
+}
+
+function create(req, res) {
+  Skill.create(req.body)
+  res.redirect('/skills');
+}
+
+function deleteSkill(req, res) {
+  Skill.deleteOne(req.params.id);
+  res.redirect('/skills');
+}
+
+function edit(req, res){
+  const skill = Skill.getOne(req.params.id);
+  res.render('skills/edit', {
+    title: 'Edit Skill',
+    skill
+  })
+}
+
+function update(req, res){
+  req.body.done = !!req.body.done;
+  Skill.update(req.params.id, req.body);
+  res.redirect(`/skills/${req.params.id}`);
 }
 
   module.exports = {
     index,
-    show
+    show,
+    new: newSkill,
+    create,
+    delete: deleteSkill,
+    edit,
+    update
   };
 	
